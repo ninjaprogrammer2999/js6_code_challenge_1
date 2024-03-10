0aeedd55334eb97d403d3dc6d2b75be790ef2c44
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-03-10 22:45:07 +0530

    javascript
    chapter 5: first code challenge!

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// code challenge!
+// write code that will return a random letter from your name
+
+//let myName = "Dude";
+//console.log(myName.charAt(2));
+// I need random from 0 to 3 
+
+//console.log(Math.floor(Math.random()*4));
+//console.log(Math.floor(Math.random()*4));
+//console.log(Math.floor(Math.random()*4));
+//console.log(Math.floor(Math.random()*4));
+// so now we have what we need 
+//console.log(myName.charAt(Math.floor(Math.random()*4)));
+//console.log(myName.charAt(Math.floor(Math.random()*4)));
+//console.log(myName.charAt(Math.floor(Math.random()*4)));
+//console.log(myName.charAt(Math.floor(Math.random()*4)));
+/***********************************************************/
+// I need this code to work for anyname! how you gonnna do it?
+/**************** */
+// let's have a variable that stores anyname!!
+let anyName = "abcdefghijklmnopqrstuvwxyz";
+/* let me check the length of anyName ofcourse 
+it should 26 lol but still let's check xd*/
+console.log(anyName.length);
+// so now i can return any letter from this!!
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+
+
+
+
+
